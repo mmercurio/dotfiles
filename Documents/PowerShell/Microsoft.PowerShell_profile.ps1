b52c349cb39c3b0680b15682e3306bed1d041216
Michael Mercurio <michael@mmercurio.com>
2021-10-31 00:15:29 -0400

    PowerShell profile

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,12 @@
-# https://hodgkins.io/ultimate-powershell-prompt-and-git-setup
+# https://www.hanselman.com/blog/my-ultimate-powershell-prompt-with-oh-my-posh-and-the-windows-terminal
 
-Import-Module -Name posh-git
-Import-Module -Name oh-my-posh
-Set-Theme Paradox
-Import-Module -Name PSColor
+Import-Module -Name Terminal-Icons
+Import-Module -Name PSReadLine
+Import-Module oh-my-posh
+
+oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
+
+Set-PoshPrompt -Theme Paradox
 
 # Ensure git uses OpenSSH instead of the ssh that comes with git
 # This can be set using global git config: core.sshcommand
---
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,49 @@
+
+# https://hodgkins.io/ultimate-powershell-prompt-and-git-setup
+
+Import-Module -Name posh-git
+#Import-Module -Name PSColor # This makes colored dir output very slow on Windows PowerShell v5.x
+
+function Test-Administrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+function prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+
+    Write-Host
+
+    if (Test-Administrator) {  # Use different username if elevated
+        Write-Host "[Admin] " -NoNewline -ForegroundColor Red
+    }
+
+    #Write-Host "$ENV:USERNAME@" -NoNewline -ForegroundColor DarkYellow
+    #Write-Host "$ENV:COMPUTERNAME" -NoNewline -ForegroundColor Magenta
+
+    if ($null -ne $s) {  # color for PSSessions
+        Write-Host " (`$s: " -NoNewline -ForegroundColor DarkGray
+        Write-Host "$($s.Name)" -NoNewline -ForegroundColor Yellow
+        Write-Host ") " -NoNewline -ForegroundColor DarkGray
+    }
+
+    #Write-Host " : " -NoNewline -ForegroundColor DarkGray
+    Write-Host $($(Get-Location) -replace ($env:USERPROFILE).Replace('\','\\'), "~") -NoNewline -ForegroundColor Blue
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+    Write-Host (Get-Date -Format G) -NoNewline -ForegroundColor DarkMagenta
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+
+    $global:LASTEXITCODE = $realLASTEXITCODE
+
+    Write-VcsStatus
+
+    Write-Host ""
+
+    return "PS> "
+}
+
+# Function to run as Admin:
+function Invoke-RunAsAdmin { Start-Process -Verb RunAs (Get-Process -Id $PID).Path }
+
+# Alias for the function:
+  Set-Alias psadmin Invoke-RunAsAdmin
