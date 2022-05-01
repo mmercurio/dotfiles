b3647cc64adc394f76f269de4a84edd60f35a171
Michael Mercurio <michael@mmercurio.com>
2022-05-01 09:36:35 -0400

    finicky & browserosaurus

---
+++ home/dot_finicky.js
@@ -0,0 +1,36 @@
+const GH = "Microsoft Edge"
+const PERSONAL = "Safari"
+
+module.exports = {
+  defaultBrowser: "Browserosaurus", // "Safari"
+  handlers: [
+    {
+      match: [
+        "*.grubhub.com*", "grubhub.com*",
+        "*.heroku.com/*",
+        "*.datadoghq.com/*",
+        "*.newrelic.com/*",
+        "*github.com/TheLevelUp/*", "*github.com/GrubHubProd/*",
+        "grubhub.okta.com",
+        "levelup.atlassian.net/*"
+      ],
+      browser: GH
+    },
+    {
+      match: [
+        "*github.com/mmercurio/*",
+        "*.icloud.com*",
+        "*.cloudspotter.org*"
+      ],
+      browser: PERSONAL
+    },
+    {
+      // You can get the path of the process that triggered Finicky
+      match: ({ sourceProcessPath }) =>
+        sourceProcessPath &&
+        (sourceProcessPath.startsWith("/Applications/Zulip.app") ||
+         sourceProcessPath.startsWith("/Applications/Slack.app")),
+     browser: GH
+    },
+  ]
+}
