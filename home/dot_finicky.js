52c145361b8a1322eb0760814f75bcbf8aa73a7c
Michael Mercurio <michael@mmercurio.com>
2025-02-13 16:20:47 -0500

    aws urls

--- home/dot_finicky.js
+++ home/dot_finicky.js
@@ -24,6 +24,7 @@ module.exports = {
         "github.com/TheLevelUp/*", "github.com/GrubHubProd/*",
         "*.github.com/TheLevelUp/*", "*.github.com/GrubHubProd/*",
         "grubhub.slack.com/*",
+        "*.amazonaws.com/*"
       ],
       browser: GH,
     },
