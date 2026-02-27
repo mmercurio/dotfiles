10b8382731ef4dddcd993aa72ec572b3fc09303e
Michael Mercurio <mmercurio@users.noreply.github.com>
2026-02-26 23:50:13 -0500

    Wezterm on macOS

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -7,7 +7,9 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 config.font = wezterm.font("MesloLGS NF")
-config.font_size = 10
+config.font_size = 14
+config.initial_cols = 100
+config.initial_rows = 28
 
 -- config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
@@ -17,7 +19,7 @@ config.hide_tab_bar_if_only_one_tab = true
 --   "TITLE" - disable the resizable border and enable only the title bar
 --   "RESIZE" - disable the title bar but enable the resizable border
 --   "TITLE | RESIZE" - Enable titlebar and border. This is the default.
-config.window_decorations = "RESIZE"
+config.window_decorations = "TITLE | RESIZE"
 
 config.window_background_opacity = 0.95
 
@@ -42,6 +44,20 @@ config.mouse_bindings = {
                 end),
         },
 }
+
+-- https://github.com/dfaerch/passrelay.wezterm
+-- https://github.com/dfaerch/passrelay.wezterm/blob/v1/integrations/1password_desktop.md
+local passrelay_settings = {
+  get_userlist = {
+    format='json',
+    command = "~/bin/op item list --tags wezterm --format=json",
+    id_path = "id",
+    label_path = "title"
+  },
+  get_password = "~/bin/op item get %user --fields password --reveal",
+  hotkey = { mods = 'CTRL', key = 'p' },
+}
+wezterm.plugin.require("https://github.com/dfaerch/passrelay.wezterm").apply_to_config(config, passrelay_settings)
 
 -- and finally, return the configuration to wezterm
 return config
