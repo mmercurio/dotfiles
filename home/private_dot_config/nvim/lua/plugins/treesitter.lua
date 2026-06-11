e6418a25c76b0deae4802e47597a635f284d1987
Michael Mercurio <mmercurio@users.noreply.github.com>
2026-06-10 23:34:44 -0400

    More nvim plugins
    Based on https://www.josean.com/posts/how-to-setup-neovim-2024
    with updates to fix issues and some minor customizations.

---
+++ home/private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,55 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
+  config = function()
+    -- import nvim-treesitter plugin
+    local treesitter = require("nvim-treesitter.config")
+
+    -- configure treesitter
+    treesitter.setup({ -- enable syntax highlighting
+      highlight = {
+        enable = true,
+      },
+      -- enable indentation
+      indent = { enable = true },
+      -- enable autotagging (w/ nvim-ts-autotag plugin)
+      autotag = {
+        enable = true,
+      },
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        "json",
+        "java",
+        "javascript",
+        "typescript",
+        "yaml",
+        "html",
+        "css",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "bash",
+        "lua",
+        "vim",
+        "dockerfile",
+        "gitignore",
+        "query",
+        "c",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
+  end,
+}
