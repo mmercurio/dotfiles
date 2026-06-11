f118c1e1f365754c84e256cb752b8e9bfa41dcab
Michael Mercurio <mmercurio@users.noreply.github.com>
2026-06-10 23:34:44 -0400

    More nvim plugins
    Based on https://www.josean.com/posts/how-to-setup-neovim-2024
    with updates to fix issues and some minor customizations.

---
+++ home/private_dot_config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,22 @@
+return {
+  -- "numToStr/Comment.nvim",
+  --    patched for nvim 0.12 until PR is merged: 
+  --    https://github.com/numToStr/Comment.nvim/pull/521
+  "neovim-plugins/comment.nvim",
+  branch = "fix/patch-treesitter",
+
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
+  config = function()
+    -- import comment plugin safely
+    local comment = require("Comment")
+
+    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+
+    comment.setup({
+      pre_hook = ts_context_commentstring.create_pre_hook(),
+    })
+  end,
+}
