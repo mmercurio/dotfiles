e780b790365c3fb822c5c041db99132c9ee726ff
Michael Mercurio <mmercurio@users.noreply.github.com>
2026-06-13 23:43:16 -0400

    initial LSP config
    lua_ls and pyright working

--- home/private_dot_config/nvim/lua/plugins/telescope.lua
+++ home/private_dot_config/nvim/lua/plugins/telescope.lua
@@ -1,36 +1,50 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-
-    telescope.setup({
-      defaults = {
-        path_display = { "smart" },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+    },
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+        telescope.setup({
+          defaults = {
+            path_display = { "smart" },
+            mappings = {
+              i = {
+                ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+                ["<C-j>"] = actions.move_selection_next, -- move to next result
+                ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+              },
+            },
           },
-        },
-      },
-    })
+        })
+      telescope.load_extension("fzf")
 
-    telescope.load_extension("fzf")
+      -- set keymaps
+      local keymap = vim.keymap
+      local builtin = require('telescope.builtin')
 
-    -- set keymaps
-    local keymap = vim.keymap
-    local builtin = require('telescope.builtin')
-
-    keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep in cwd" })
-    keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Find string under cursor in cwd" })
-  end,
+      keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
+      keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
+      keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep in cwd" })
+      keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Find string under cursor in cwd" })
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+          }
+        }
+      })
+      require("telescope").load_extension("ui-select")
+    end
+  },
 }
