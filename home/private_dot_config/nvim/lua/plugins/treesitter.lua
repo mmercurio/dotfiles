0c4b27aed189164355f64f5614b3933abb904624
Michael Mercurio <mmercurio@users.noreply.github.com>
2026-06-15 18:03:38 -0400

    More treesitter language parsers

--- home/private_dot_config/nvim/lua/plugins/treesitter.lua
+++ home/private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -7,4 +7,24 @@ return {
   dependencies = {
     "windwp/nvim-ts-autotag",
   },
+  config = function()
+    require("nvim-treesitter").install({
+      "lua",
+      "bash",
+      "html",
+      "java",
+      "javascript",
+      "json",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "regex",
+      "ruby",
+      "vim",
+      "yaml",
+    })
+
+    -- Autoclose HTML tags
+    require("nvim-ts-autotag").setup()
+  end,
 }
