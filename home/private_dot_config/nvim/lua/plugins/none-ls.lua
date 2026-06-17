a5c13c167d3c3e13899a76526d4e26ae5ba7ce6e
Michael Mercurio <mmercurio@users.noreply.github.com>
2026-06-17 00:00:43 -0400

    WIP completions with lsp and snippets
    Some of this is working. But not sure if everything
    is working.

--- home/private_dot_config/nvim/lua/plugins/none-ls.lua
+++ home/private_dot_config/nvim/lua/plugins/none-ls.lua
@@ -1,10 +1,14 @@
 return {
   "nvimtools/none-ls.nvim",
+  -- null-ls wraps CLI tools for use with lsp such as formatters.
+  -- This allows CLI formatting tools to be used as LSP formatters.
+  -- e.g., for Python, we could use isort and black here.
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua, -- stylua config is stylua.toml
+        null_ls.builtins.formatting.prettier,
       },
     })
 
