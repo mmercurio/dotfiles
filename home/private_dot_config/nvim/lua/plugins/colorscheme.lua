51f6de3c026b95f01affe898507758bb6bba91f2
Michael Mercurio <mmercurio@users.noreply.github.com>
2026-08-25 19:59:31 -0400

    colorscheme transparent background

--- home/private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -5,10 +5,10 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+      })
       vim.cmd.colorscheme("catppuccin-mocha")
-
-      -- Apply transparency to the main editor background
-      vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
 
       vim.api.nvim_set_hl(0, "CursorColumn", { bg = "#2a2b3c" })
     end,
